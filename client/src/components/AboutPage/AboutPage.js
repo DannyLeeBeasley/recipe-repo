6fee3309c5ef82174f5076057defe9bb2ee6634d
Dan Beasley <dannyleebeasley@gmail.com>
2021-11-30 10:18:41 -0600

    changed name of project and repo

--- client/src/components/AboutPage/AboutPage.js
+++ client/src/components/AboutPage/AboutPage.js
@@ -5,7 +5,7 @@ function AboutPage() {
   return (
     <div className="about-page-container">
     <p className="about-page-text">
-      Recipe Repo was created to help family, friends, and strangers share the love of food, cooking, and culture.
+      Recipe Repo was created to help family, friends, and strangers share the love of food, cooking, and culture. Share your recipes freely.
     </p>
     <p className="about-page-text">
        Enjoy!
