669c746911be4950d3dc8bdffda2e985591ca9a7
Dan Beasley <dannyleebeasley@gmail.com>
2021-12-01 21:28:33 -0600

    more css

--- client/src/components/AboutPage/AboutPage.js
+++ client/src/components/AboutPage/AboutPage.js
@@ -4,12 +4,15 @@ import "./AboutPage.css";
 function AboutPage() {
   return (
     <div className="about-page-container">
-    <p className="about-page-text">
-      Recipe Repo was created to help family, friends, and strangers share the love of food, cooking, and culture. Share your favorite recipes here and explore the recipes shared by others.
-    </p>
-    <p className="about-page-text">
-       Enjoy!
-    </p>
+      <p className="about-page-text">
+        Recipe Repo was created to help family, friends, and strangers share the
+        love of food, cooking, and culture.
+      </p>
+      <p className="about-page-text">
+        Share your favorite recipes here and explore the recipes shared by
+        others.
+      </p>
+      <p className="about-page-text">Enjoy!</p>
     </div>
   );
 }
--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { NavBarItems } from "./NavBarItems";
 import { Link } from "react-router-dom";
 import { useState } from "react/cjs/react.development";
